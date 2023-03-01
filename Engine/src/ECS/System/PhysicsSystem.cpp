3c8c70d73202e5b9f00253aa06a688405c1336bd
Luke Burton <lukesburton19@gmail.com>
2023-03-01 11:46:58 -0700

    adds rigidbody velocity, random float generation

--- Engine/src/ECS/System/PhysicsSystem.cpp
+++ Engine/src/ECS/System/PhysicsSystem.cpp
@@ -1,5 +1,6 @@
 #include "PhysicsSystem.hpp"
 #include "Transform.hpp"
+#include "RigidBody.hpp"
 #include <iostream>
 
 PhysicsSystem::PhysicsSystem()
@@ -10,5 +11,20 @@ PhysicsSystem::PhysicsSystem()
 void PhysicsSystem::update(std::shared_ptr<entt::registry> registry, float deltaTime)
 {
     mElapsedTime += deltaTime;
-    auto view = registry->view<Transform>();
+    auto view = registry->view<Transform, RigidBody>();
+
+    for (auto entity : view)
+    {
+        auto& rb = view.get<RigidBody>(entity);
+        auto& transform = view.get<Transform>(entity);
+
+        rb.velocity.y += 9.8f * deltaTime;
+
+        transform.position.x += rb.velocity.x * deltaTime;
+        transform.position.y += rb.velocity.y * deltaTime;
+        transform.position.z += rb.velocity.z * deltaTime;
+
+    }
+
+
 }
