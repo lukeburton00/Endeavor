5b0d2746ea812a07145fea290abc89c31d59d767
Luke Burton <lukesburton19@gmail.com>
2023-09-17 16:46:05 -0600

    remove asset manager. update include categories

--- endeavor/endeavor.hpp
+++ endeavor/endeavor.hpp
@@ -3,11 +3,12 @@
 /* ----- Core ----- */
 #include "input/input.hpp"
 #include "time/time.hpp"
+
+/* ----- Math ----- */
 #include "math/random.hpp"
 #include "math/noise/perlin_noise.hpp"
 
 /* ----- Graphics ----- */
 #include "graphics/window.hpp"
-#include "graphics/asset_manager.hpp"
 #include "graphics/sprite_batch.hpp"
 #include "graphics/camera.hpp"
