f7e02cad6476b685a50f85805f698b7b619a2797
Luke Burton <lukesburton19@gmail.com>
2024-01-02 21:42:15 -0700

    add renderer singleton calls

--- endeavor/src/core/game.cpp
+++ endeavor/src/core/game.cpp
@@ -1,32 +1,32 @@
 #include "game.hpp"
 
+#include "renderer/renderer.hpp"
+
 #include <thread>
 #include <chrono>
 #include <iostream>
 
 namespace Endeavor
 {
+    Game* Game::s_Instance = nullptr;
+
     Game::Game()
     : m_Window(WindowProperties("Endeavor", 800, 600))
     {
-
-    }
-
-    Game::~Game()
-    {
-
+        s_Instance = this;
     }
 
     void Game::Run()
     {
         m_Window.Init();
+        Renderer::GetInstance()->Init();
 
         while (!m_Window.quit)
         {
             m_Window.Update();
             Update();
 
-            m_Window.Clear(1,1,1);
+            m_Window.Clear(0.f, 0.f, 0.f);
             Render();
             m_Window.SwapBuffers();
         }
