57b40eb6a5887c595e61d334951420dc92f8bd25
Luke Burton <lukesburton19@gmail.com>
2024-01-02 21:43:23 -0700

    update private variables to match project style

--- endeavor/src/renderer/camera.cpp
+++ endeavor/src/renderer/camera.cpp
@@ -10,20 +10,18 @@ namespace Endeavor
         position = glm::vec2(0,0);
         width = w;
         height = h;
-        mView = glm::mat4(1.0f);
+        m_View = glm::mat4(1.0f);
         Update();
     }
 
     void Camera::Update()
     {
-        mProjection = glm::ortho(-width / 2, width / 2, height / 2, -height / 2, -1.f, 1.f);
-        //std::cout << "Camera position X: " << position.x << " Camera position Y: " << position.y << std::endl;
-        mView = glm::mat4(1.f);
-        mView = glm::translate(mView, glm::vec3(-position.x, -position.y, 0.f));
+        m_Projection = glm::ortho(-width / 2, width / 2, height / 2, -height / 2, -1.f, 1.f);
+        m_View = glm::translate(glm::mat4(1.f), glm::vec3(-position.x, -position.y, 0.f));
     }
 
     glm::vec2 Camera::GetWorldFromScreen(glm::vec2 screenPos, glm::vec2 screenDimensions)
     {
-        return glm::unProject(glm::vec3(screenPos.x, screenPos.y, 0.f), mView, mProjection, glm::vec4(0.f, screenDimensions.y, screenDimensions.x, -screenDimensions.y));
+        return glm::unProject(glm::vec3(screenPos.x, screenPos.y, 0.f), m_View, m_Projection, glm::vec4(0.f, screenDimensions.y, screenDimensions.x, -screenDimensions.y));
     }
 }
--- endeavor/src/renderer/camera.hpp
+++ endeavor/src/renderer/camera.hpp
@@ -8,9 +8,9 @@ namespace Endeavor
     {
     public:
         Camera(glm::vec2 pos = glm::vec2(0.0f, 0.0f), float width = 800, float height = 600);
-        glm::mat4& GetViewMatrix() { return mView; }
-        glm::mat4& GetProjectionMatrix() { return mProjection; }
-        glm::vec2 GetWorldFromScreen (glm::vec2 screenPos, glm::vec2 screenDimensions);
+        glm::mat4& GetViewMatrix() { return m_View; }
+        glm::mat4& GetProjectionMatrix() { return m_Projection; }
+        glm::vec2 GetWorldFromScreen(glm::vec2 screenPos, glm::vec2 screenDimensions);
         void Update();
 
     public:
@@ -19,7 +19,7 @@ namespace Endeavor
         glm::vec2 position = glm::vec2(0.f);
 
     private:
-        glm::mat4 mProjection;
-        glm::mat4 mView;
+        glm::mat4 m_Projection;
+        glm::mat4 m_View;
     };
 }
