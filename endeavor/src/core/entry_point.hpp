0d48710d7206179990efa624cf8d72539dabbdb7
Luke Burton <lukesburton19@gmail.com>
2024-01-02 21:41:23 -0700

    update styling and explicit entry point return

--- endeavor/src/core/entry_point.hpp
+++ endeavor/src/core/entry_point.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
 #include "game.hpp"
+
 #include <iostream>
 
 extern Endeavor::Game* Endeavor::CreateGame();
 
 int main(int argc, char** argv)
 {
-    std::cout << "Testing" << std::endl;
     Endeavor::Game* game = Endeavor::CreateGame();
     game->Run();
     delete game;
+
+    return 0;
 }
